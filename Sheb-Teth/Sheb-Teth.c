236e3984f6923dc5f36a48d0cfb6675927fa3179
emptymonkey <pseudorandom@gmail.com>
2017-03-13 14:02:55 -0700

    Began asm template / testing.

--- Sheb-Teth/Sheb-Teth.c
+++ Sheb-Teth/Sheb-Teth.c
@@ -41,6 +41,8 @@
  *
  **********************************************************************************************************************/
 
+// GUID's are 36 chars with dashes. e.g. 4DEF757C-321A-4007-84FE-5AF5EA607BD4
+#define BUFFER_LEN	36
 
 int xor_decrypt_and_compare(char *query, char *cipher, char *key);
 
@@ -66,3 +68,53 @@ int main(int argc, char **argv, char **envp){
 
 	return(0);
 }
+
+int xor_decrypt_and_compare(char *query, char *cipher, char *key){
+
+
+	return(0);
+}
+
+
+void eldritch_function(){
+/*
+	 $ head -c 36 </dev/urandom | ./udcli -att
+	 0000000000000000 90               nop                     
+	 0000000000000001 fb               sti                     
+	 0000000000000002 1c7b             sbb $0x7b, %al          
+	 0000000000000004 7503             jnz 0x9                 
+	 0000000000000006 9ab5c2835401c0   call $0xc001, $0x5483c2b5
+	 000000000000000d 44               inc %esp                
+	 000000000000000e ca8a8f           lret  $0x8f8a           
+	 0000000000000011 a36276cf23       mov %eax, 0x23cf7662    
+	 0000000000000016 3b3e             cmp (%esi), %edi        
+	 0000000000000018 0b39             or (%ecx), %edi         
+	 000000000000001a 7778             ja 0x94                 
+	 000000000000001c 2b4c1ae8         sub -0x18(%edx,%ebx), %ecx
+	 0000000000000020 d7               xlatb                   
+	 0000000000000021 97               xchg %eax, %edi         
+	 0000000000000022 b7d1             mov $0xd1, %bh
+ */
+
+
+	// key -> \x90\xfb\x1c\x7b\x75\x03\x9a\xb5\xc2\x83\x54\x01\xc0\x44\xca\x8a\x8f\xa3\x62\x76\xcf\x23\x3b\x3e\x0b\x39\x77\x78\x2b\x4c\x1a\xe8\xd7\x97\xb7\xd1
+
+	__asm__ __volitile__("
+			nop
+			sti
+			sbb $0x7b, %%al
+			jnz 0x9
+			call $0xc001, $0x5483c2b5
+			inc %%esp
+			lret  $0x8f8a
+			mov %%eax, 0x23cf7662
+			cmp (%%esi), %%edi
+			or (%%ecx), %%edi
+			ja 0x94
+			sub -0x18(%%edx,%%ebx), %%ecx
+			xlatb
+			xchg %%eax, %%edi
+			mov $0xd1, %%bh
+			");
+}
+
